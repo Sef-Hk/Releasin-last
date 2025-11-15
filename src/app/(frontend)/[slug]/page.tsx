695f0cc69b42f0c210ab2058629ebb2ee1779f26
Sef-Hk <ahk.sefsaf@gmail.com>
2025-11-15 07:29:15 +0100

    sfsgwa

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -1,3 +1,118 @@
+// import type { Metadata } from 'next'
+
+// import { PayloadRedirects } from '@/components/PayloadRedirects'
+// import configPromise from '@payload-config'
+// import { getPayload, type RequiredDataFromCollectionSlug } from 'payload'
+// import { draftMode } from 'next/headers'
+// import React, { cache } from 'react'
+// import { homeStatic } from '@/endpoints/seed/home-static'
+
+// import { RenderBlocks } from '@/blocks/RenderBlocks'
+// import { RenderHero } from '@/heros/RenderHero'
+// import { generateMeta } from '@/utilities/generateMeta'
+// import PageClient from './page.client'
+// import { LivePreviewListener } from '@/components/LivePreviewListener'
+
+// // export async function generateStaticParams() {
+// //   const payload = await getPayload({ config: configPromise })
+// //   const pages = await payload.find({
+// //     collection: 'pages',
+// //     draft: false,
+// //     limit: 1000,
+// //     overrideAccess: false,
+// //     pagination: false,
+// //     select: {
+// //       slug: true,
+// //     },
+// //   })
+
+// //   const params = pages.docs
+// //     ?.filter((doc) => {
+// //       return doc.slug !== 'home'
+// //     })
+// //     .map(({ slug }) => {
+// //       return { slug }
+// //     })
+
+// //   return params
+// // }
+
+// type Args = {
+//   params: Promise<{
+//     slug?: string
+//   }>
+// }
+
+// export default async function Page({ params: paramsPromise }: Args) {
+//   const { isEnabled: draft } = await draftMode()
+//   const { slug = 'home' } = await paramsPromise
+//   // Decode to support slugs with special characters
+//   const decodedSlug = decodeURIComponent(slug)
+//   const url = '/' + decodedSlug
+//   let page: RequiredDataFromCollectionSlug<'pages'> | null
+
+//   page = await queryPageBySlug({
+//     slug: decodedSlug,
+//   })
+
+//   // Remove this code once your website is seeded
+//   if (!page && slug === 'home') {
+//     page = homeStatic
+//   }
+
+//   if (!page) {
+//     return <PayloadRedirects url={url} />
+//   }
+
+//   const { hero, layout } = page
+
+//   return (
+//     <article className="">
+//       <PageClient />
+//       {/* Allows redirects for valid pages too */}
+//       <PayloadRedirects disableNotFound url={url} />
+
+//       {draft && <LivePreviewListener />}
+
+//       <RenderHero {...hero} />
+//       <RenderBlocks blocks={layout} />
+//     </article>
+//   )
+// }
+
+// export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+//   const { slug = 'home' } = await paramsPromise
+//   // Decode to support slugs with special characters
+//   const decodedSlug = decodeURIComponent(slug)
+//   const page = await queryPageBySlug({
+//     slug: decodedSlug,
+//   })
+
+//   return generateMeta({ doc: page })
+// }
+
+// const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
+//   const { isEnabled: draft } = await draftMode()
+
+//   const payload = await getPayload({ config: configPromise })
+
+//   const result = await payload.find({
+//     collection: 'pages',
+//     draft,
+//     limit: 1,
+//     pagination: false,
+//     overrideAccess: draft,
+//     where: {
+//       slug: {
+//         equals: slug,
+//       },
+//     },
+//   })
+
+//   return result.docs?.[0] || null
+// })
+
+
 import type { Metadata } from 'next'
 
 import { PayloadRedirects } from '@/components/PayloadRedirects'
@@ -5,37 +120,12 @@ import configPromise from '@payload-config'
 import { getPayload, type RequiredDataFromCollectionSlug } from 'payload'
 import { draftMode } from 'next/headers'
 import React, { cache } from 'react'
-import { homeStatic } from '@/endpoints/seed/home-static'
 
 import { RenderBlocks } from '@/blocks/RenderBlocks'
 import { RenderHero } from '@/heros/RenderHero'
 import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
-
-// export async function generateStaticParams() {
-//   const payload = await getPayload({ config: configPromise })
-//   const pages = await payload.find({
-//     collection: 'pages',
-//     draft: false,
-//     limit: 1000,
-//     overrideAccess: false,
-//     pagination: false,
-//     select: {
-//       slug: true,
-//     },
-//   })
-
-//   const params = pages.docs
-//     ?.filter((doc) => {
-//       return doc.slug !== 'home'
-//     })
-//     .map(({ slug }) => {
-//       return { slug }
-//     })
-
-//   return params
-// }
 
 type Args = {
   params: Promise<{
@@ -46,19 +136,14 @@ type Args = {
 export default async function Page({ params: paramsPromise }: Args) {
   const { isEnabled: draft } = await draftMode()
   const { slug = 'home' } = await paramsPromise
+
   // Decode to support slugs with special characters
   const decodedSlug = decodeURIComponent(slug)
   const url = '/' + decodedSlug
-  let page: RequiredDataFromCollectionSlug<'pages'> | null
 
-  page = await queryPageBySlug({
+  const page: RequiredDataFromCollectionSlug<'pages'> | null = await queryPageBySlug({
     slug: decodedSlug,
   })
-
-  // Remove this code once your website is seeded
-  if (!page && slug === 'home') {
-    page = homeStatic
-  }
 
   if (!page) {
     return <PayloadRedirects url={url} />
@@ -67,7 +152,7 @@ export default async function Page({ params: paramsPromise }: Args) {
   const { hero, layout } = page
 
   return (
-    <article className="">
+    <article>
       <PageClient />
       {/* Allows redirects for valid pages too */}
       <PayloadRedirects disableNotFound url={url} />
@@ -82,8 +167,8 @@ export default async function Page({ params: paramsPromise }: Args) {
 
 export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
   const { slug = 'home' } = await paramsPromise
-  // Decode to support slugs with special characters
   const decodedSlug = decodeURIComponent(slug)
+
   const page = await queryPageBySlug({
     slug: decodedSlug,
   })
@@ -93,7 +178,6 @@ export async function generateMetadata({ params: paramsPromise }: Args): Promise
 
 const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
   const { isEnabled: draft } = await draftMode()
-
   const payload = await getPayload({ config: configPromise })
 
   const result = await payload.find({
@@ -103,9 +187,7 @@ const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
     pagination: false,
     overrideAccess: draft,
     where: {
-      slug: {
-        equals: slug,
-      },
+      slug: { equals: slug },
     },
   })
 
