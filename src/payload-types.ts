cdce3ce9d6c2619b9fc838712d1934d8ddbeac3a
Sef-Hk <ahk.sefsaf@gmail.com>
2025-11-15 07:34:35 +0100

    remove the seed

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -311,6 +311,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   folder?: (number | null) | FolderInterface;
   updatedAt: string;
   createdAt: string;
@@ -1621,6 +1622,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   folder?: T;
   updatedAt?: T;
   createdAt?: T;
