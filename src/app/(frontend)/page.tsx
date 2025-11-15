fa0b36f01f612f10ad8dfb34d3cdec84502873ec
Sef-Hk <ahk.sefsaf@gmail.com>
2025-11-15 06:30:47 +0100

    sfsfsss

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,6 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
-
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 export default PageTemplate
 
 export { generateMetadata }
