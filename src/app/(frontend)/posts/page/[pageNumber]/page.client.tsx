a48bc535217437bfa5e8719b8dd96bccaebf7ef1
Sef-Hk <ahk.sefsaf@gmail.com>
2025-11-15 06:06:09 +0100

    sfsgeee

--- src/app/(frontend)/posts/page/[pageNumber]/page.client.tsx
+++ src/app/(frontend)/posts/page/[pageNumber]/page.client.tsx
@@ -1,6 +1,7 @@
-export const dynamic = 'force-dynamic';
 
 'use client'
+export const dynamic = 'force-dynamic';
+
 import { useHeaderTheme } from '@/providers/HeaderTheme'
 import React, { useEffect } from 'react'
 
